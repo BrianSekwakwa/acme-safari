bbf33c0ec58ea0474486828d9c039834f21da7ff
Brian Sekwakwa <briansenpai14@gmail.com>
2019-04-06 09:22:05 +0200

    completed contact form with spinner animation

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,25 @@
-// Responsive Menu
+// Initializing variables
+const form = document.querySelector(".contact__form form");
+const contactForm = document.querySelector(".contact__form");
+const spinner = document.querySelector(".contact__spinner");
+const message = document.querySelector(".contact__message");
 
+// Smooth Scrolling
+
+// Responsive Menu
 $(".handle").on("click", function() {
   $("nav ul").toggleClass("showing");
 });
+
+// Form event on submit
+form.addEventListener("submit", load);
+
+function load(e) {
+  contactForm.style.display = "none";
+  spinner.style.display = "block";
+  setTimeout(() => {
+    spinner.style.display = "none";
+    message.style.display = "block";
+  }, 2000);
+  e.preventDefault();
+}
