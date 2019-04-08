08231712a3dd0a3cfcd29de4d3cc1cdae30575d9
Brian Sekwakwa <briansenpai14@gmail.com>
2019-04-08 17:34:55 +0200

    fixed the modal section and added finishing touches to the website

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,12 +4,12 @@ const contactForm = document.querySelector(".contact__form");
 const spinner = document.querySelector(".contact__spinner");
 const message = document.querySelector(".contact__message");
 const modal = document.querySelector(".modal");
-const modalContent = document.querySelector(".modal__content");
+const modalContent = document.querySelector(".modal__box__content");
 const modalCloseButton = document.querySelector(".modal__box__close");
 const image1 = document.querySelector("#image01");
-// const image2 = document.querySelector("#image02");
-// const image3 = document.querySelector("#image03");
-// const image4 = document.querySelector("#image04");
+const image2 = document.querySelector("#image02");
+const image3 = document.querySelector("#image03");
+const image4 = document.querySelector("#image04");
 
 // Smooth Scrolling
 
@@ -31,58 +31,30 @@ function load(e) {
   e.preventDefault();
 }
 
-// // Click function for show the Modal
-
-// $(".show").on("click", function() {
-//   $(".mask").addClass("active");
-// });
-
-// // Function for close the Modal
-
-// function closeModal() {
-//   $(".mask").removeClass("active");
-// }
-
-// // Call the closeModal function on the clicks/keyboard
-
-// $(".close, .mask").on("click", function() {
-//   closeModal();
-// });
-
-// $(document).keyup(function(e) {
-//   if (e.keyCode == 27) {
-//     closeModal();
-//   }
-// });
-
-// // Puttin Images into the modal
-// image1.addEventListener("click", loadImage);
-// image2.addEventListener("click", loadImage);
-// image3.addEventListener("click", loadImage);
-// image4.addEventListener("click", loadImage);
-
-// function loadImage(e) {
-//   if (e.target.parentNode.id === "image01") {
-//     modalContent.innerHTML = "<h1>Number One</h1>";
-//   }
-//   if (e.target.parentNode.id === "image02") {
-//     modalContent.innerHTML = "<h1>Number Two</h1>";
-//   }
-//   if (e.target.parentNode.id === "image03") {
-//     modalContent.innerHTML = "<h1>Number Three</h1>";
-//   }
-//   if (e.target.parentNode.id === "image04") {
-//     modalContent.innerHTML = "<h1>Number One</h1>";
-//   }
-// }
-
 // Opening Modal
 
 image1.addEventListener("click", openModal);
+image2.addEventListener("click", openModal);
+image3.addEventListener("click", openModal);
+image4.addEventListener("click", openModal);
 
 function openModal(e) {
-  console.log("step");
   modal.classList.add("showModal");
+
+  console.log(e.target.parentNode.id);
+  // loading specific images into the modal
+  if (e.target.parentNode.id === "image01") {
+    modalContent.innerHTML = "";
+  }
+  if (e.target.parentNode.id === "image02") {
+    modalContent.innerHTML = "<h1>Number Two</h1>";
+  }
+  if (e.target.parentNode.id === "image03") {
+    modalContent.innerHTML = "<h1>Number Three</h1>";
+  }
+  if (e.target.parentNode.id === "image04") {
+    modalContent.innerHTML = "<h1>Number One</h1>";
+  }
 }
 
 // Close Modal
@@ -91,3 +63,10 @@ modalCloseButton.addEventListener("click", closeModal);
 function closeModal(e) {
   modal.classList.remove("showModal");
 }
+
+// Close modal with an Esc key
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+    modal.classList.remove("showModal");
+  }
+});
