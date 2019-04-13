ea02f073d0a0e64f60af577fce4534474d1736b7
Brian Sekwakwa <briansenpai14@gmail.com>
2019-04-13 20:23:11 +0200

    removed modal and put links instead with gallery hd photos

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,13 +3,6 @@ const form = document.querySelector(".contact__form form");
 const contactForm = document.querySelector(".contact__form");
 const spinner = document.querySelector(".contact__spinner");
 const message = document.querySelector(".contact__message");
-const modal = document.querySelector(".modal");
-const modalContent = document.querySelector(".modal__box__content");
-const modalCloseButton = document.querySelector(".modal__box__close");
-const image1 = document.querySelector("#image01");
-const image2 = document.querySelector("#image02");
-const image3 = document.querySelector("#image03");
-const image4 = document.querySelector("#image04");
 
 // Smooth Scrolling
 
@@ -29,60 +22,4 @@ function load(e) {
     message.style.display = "block";
   }, 2000);
   e.preventDefault();
-}
-
-// Opening Modal
-
-image1.addEventListener("click", openModal);
-image2.addEventListener("click", openModal);
-image3.addEventListener("click", openModal);
-image4.addEventListener("click", openModal);
-
-function openModal(e) {
-  modal.classList.add("showModal");
-
-  console.log(e.target.parentNode.id);
-  // loading specific images into the modal
-  if (e.target.parentNode.id === "image01") {
-    modalContent.innerHTML = "";
-  }
-  if (e.target.parentNode.id === "image02") {
-    modalContent.innerHTML = "<h1>Number Two</h1>";
-  }
-  if (e.target.parentNode.id === "image03") {
-    modalContent.innerHTML = "<h1>Number Three</h1>";
-  }
-  if (e.target.parentNode.id === "image04") {
-    modalContent.innerHTML = "<h1>Number One</h1>";
-  }
-}
-
-// Close Modal
-modalCloseButton.addEventListener("click", closeModal);
-
-function closeModal(e) {
-  modal.classList.remove("showModal");
-}
-
-// Close modal with an Esc key
-$(document).keyup(function(e) {
-  if (e.keyCode == 27) {
-    modal.classList.remove("showModal");
-  }
-});
-
-// Resizing Images
-
-window.addEventListener("resize", updateImage);
-
-function updateImage(e) {
-  if (window.innerWidth === 578) {
-    console.log("This is 578");
-  } else if (window.innerWidth === 768) {
-    console.log("this is 768");
-  } else if (window.innerWidth === 992) {
-    console.log("this is 992");
-  } else if (window.innerWidth === 1200) {
-    console.log("this is 1200");
-  }
 }
