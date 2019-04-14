6fda49af43d2c6cc2cd7cb8ecb95fc3cfd9eaa87
Brian Sekwakwa <briansenpai14@gmail.com>
2019-04-14 11:47:12 +0200

    adding pictures of offers that look good on wider phone screens and completed the gallery section

--- docs/js/index.js
+++ docs/js/index.js
@@ -5,6 +5,23 @@ const spinner = document.querySelector(".contact__spinner");
 const message = document.querySelector(".contact__message");
 
 // Smooth Scrolling
+$("body a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
 
 // Responsive Menu
 $(".handle").on("click", function() {
